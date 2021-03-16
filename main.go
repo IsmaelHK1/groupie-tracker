cc6289fc1a71b0c73c560ffc894b49eafb54ac49
ismael <ismael.hacquin@ynov.com>
2021-03-16 15:45:58 +0100

    Nouveaux design

--- main.go
+++ main.go
@@ -18,9 +18,6 @@ func serveFile() {
 	http.Handle("/main.css", fileServer)
 	http.Handle("/index.css", fileServer)
 	http.Handle("/artist.css", fileServer)
-	http.Handle("/filtre.png", fileServer)
-	http.Handle("/logo.png", fileServer)
-
 }
 
 func filters(artists, TabToPrint []structure.Artist, variable *template.Template, w http.ResponseWriter, r *http.Request) {
@@ -98,7 +95,7 @@ func handleArtist(artists []structure.Artist) {
 //runServer sets the listenandserve port to 8080
 func runServer() {
 	fmt.Println("server is runing")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8880", nil); err != nil {
 		log.Fatal(err)
 	}
 }
